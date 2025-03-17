9e003f2b06e088d416f213aae38369b8bd3466f6
sierra2001 <sierracamp09@gmail.com>
2025-03-17 13:33:24 -0400

    set up API

--- src/pages/Gyms/Gyms.jsx
+++ src/pages/Gyms/Gyms.jsx
@@ -1,8 +1,23 @@
 import Header from "../../components/Header/Header";
 import "./Gyms.scss";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "400px",
+  height: "400px",
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+};
 
 function Gyms() {
-  <Header />;
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "YOUR_API_KEY",
+  });
+
   return <h1>Find a Gym</h1>;
 }
 

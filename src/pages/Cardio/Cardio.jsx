2fa8ac28239c48507265f3c26d6a60ffa4d30e60
sierra2001 <sierracamp09@gmail.com>
2025-03-21 11:33:55 -0400

    add cardio page

--- src/pages/Cardio/Cardio.jsx
+++ src/pages/Cardio/Cardio.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Workouts from "../../data/workouts.json";
+import "./Cardio.scss";
+
+// Dynamically import all images
+const images = import.meta.glob("../../assets/images/*", { eager: true });
 
 const Cardio = () => {
+  const cardioWorkouts = Workouts.filter(
+    (workout) => workout.type === "Cardio"
+  );
+
   return (
     <div>
-      <h1>Cardio Workout</h1>
-      <p>Details about Cardio workouts...</p>
+      <div className="workout-grid">
+        {cardioWorkouts.map((workout) => {
+          const imageSrc =
+            images[`../../assets/images/${workout.image}`]?.default || "";
+
+          return (
+            <Link
+              to={`/workout/${workout.id}`}
+              key={workout.id}
+              className="workout-card"
+            >
+              <img src={imageSrc} alt={workout.name} />
+              <h3>{workout.name}</h3>
+              <p>{workout.description}</p>
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 };
